6ea8eda34f853280d9bcd0f268ab762288508fe5
Chandan8585 <chandanwingshr@gmail.com>
2023-08-16 12:59:49 +0530

    connecting db

---
+++ config/dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = async () => {
+  try {
+
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectDB;
--- hotelRouter.js
+++ hotelRouter.js
@@ -8,3 +8,5 @@ hotelRouter.route("/")
 })
 
 module.exports = hotelRouter;
+
+

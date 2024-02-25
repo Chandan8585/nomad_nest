e2f2ddc9880ea7d32b6522e2ef2d4a06eea134a9
Chandan8585 <chandanwingshr@gmail.com>
2024-02-25 15:05:55 +0530

    deploying using vercel.json file

--- index.js
+++ index.js
@@ -7,25 +7,23 @@ dotenv.config();
 
 const hotelRouter = require("./hotelRouter");
 const categoryRouter = require("./routes/category.router");
-const hotelDataAddedToDbRouter = require("./routes/dataImport.router")
-const categoryDataAddedToDbRouter = require("./routes/categoryImport.router")
+const hotelDataAddedToDbRouter = require("./routes/dataImport.router");
+const categoryDataAddedToDbRouter = require("./routes/categoryImport.router");
 const singleHotelRouter = require("./routes/singleHotel.router");
 const authRouter = require("./routes/auth.router");
 const wishListRouter = require("./routes/wishlist.router");
 const connectDB = require("./config/dbConfig");
 const app = express();
 
-
-
 const Port = 8080;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 connectDB();
 
-app.get("/" , (req, res) => {
-    res.send("hello");
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.use("/api/hotels", hotelRouter);
 app.use("/api/category", categoryRouter);
@@ -34,12 +32,9 @@ app.use("/api/categorydata", categoryDataAddedToDbRouter);
 app.use("/api/hotels", singleHotelRouter);
 app.use("/api/wishlist", wishListRouter);
 app.use("/api/auth", authRouter);
-mongoose.connection.once("open", ()=> {
-    console.log("connected to DB");
-    app.listen(process.env.PORT || Port, ()=> {
-        console.log("Server is running")
-    })
-})
-
-
-
+mongoose.connection.once("open", () => {
+  console.log("connected to DB");
+  app.listen(process.env.PORT || Port, () => {
+    console.log("Server is running");
+  });
+});
